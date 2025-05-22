fc371fbb73add19cc0ba7138a58babaeb711c884
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-05-22 12:30:16 +0200

    add comment to understand const url = new URL(req.url, );

--- exercices/ex_2/query.js
+++ exercices/ex_2/query.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 const HOST = "127.0.0.1";
 
 const server = http.createServer((req, res) => {
+/**
+ * new method to parse the URL 
+ * ${req.headers.host} -> contient l'en tête HTTP "host" de la requête.
+ * Cet en-tête indique le nom de domaine (et éventuellement le port).
+ * Si l'utilisateur visite https://monsite.com/api/data, alors req.headers.host = "monsite.com"
+ */
   const url = new URL(req.url, `http://${req.headers.host}`);
   const name = url.searchParams.get("name") || "Stranger";
   const present = url.searchParams.has("name");
@@ -16,7 +22,11 @@ const server = http.createServer((req, res) => {
     res.end("404 Not Found\n");
   }
 
-  console.log(`Request: ${req.method} ${req.url}`);
+  // console.log(`Request: ${req.method} ${req.url}`);
+  console.log(`This is a`, url.pathname);
+  console.log(`Query ${url.search}`);
+  console.log(`Name: ${name}`);
+  
 });
 
 server.listen(PORT, HOST, () => {
